55cc4ec66c2737a00c84b059fb98c39f564f45fd
Finley Cordell <finley.of.the.woods@gmail.com>
2025-10-04 13:45:35 +0100

    first commit

---
+++ src/main/java/uk/co/finleyofthewoods/warpspeed/Warpspeed.java
@@ -0,0 +1,10 @@
+package uk.co.finleyofthewoods.warpspeed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Warpspeed implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/uk/co/finleyofthewoods/warpspeed/client/WarpspeedDataGenerator.java
@@ -0,0 +1,12 @@
+package uk.co.finleyofthewoods.warpspeed.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WarpspeedDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
